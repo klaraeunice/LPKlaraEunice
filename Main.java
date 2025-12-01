12fa9f5488b05ecc0ed264bb2082f07b1b3ffde7
Clara Eunice <143270224+klaraeunice@users.noreply.github.com>
2025-12-01 00:03:41 -0300

    Cria Main do BlocoDeNotas

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+            BlocoDeNotas bloco = new BlocoDeNotas();
+        
+        
+        bloco.adicionaAnotacao("Anotar a anotação");
+        bloco.adicionaAnotacao("Comprar ração para o gato");
+        
+        System.out.println(bloco.listaAnotacao());
+        
+        bloco.editaAnotacao(1, "Estudar para a prova de OO e Java");
+        
+        bloco.removeAnotacao(2);
+        
+        System.out.println("--- Após alterações ---");
+        System.out.println(bloco.listaAnotacao());
+    }
+}
